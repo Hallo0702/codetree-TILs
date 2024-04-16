286d39eeb42de3330e01c59adafe1db2fa32fd90
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-04-16 11:08:11 +0900

    Title: 원점으로 돌아오기, Time: 245ms, Memory: 27MB, Status: Passed - CodeTree

--- 240416/원점으로 돌아오기/return-to-origin-point.py
+++ 240416/원점으로 돌아오기/return-to-origin-point.py
@@ -3,23 +3,53 @@ answer = 0
 node = []
 for i in range(N):
     x, y = map(int, input().split())
-    node.append((x,y))
+    node.append((x, y))
 
 
-def dfs(x,y,visit):
+def find_d(tp, u, v):
+    if tp == 'x':
+        if u > v:
+            return 2
+        else:
+            return 4
+    else:
+        if u > v:
+            return 1
+        else:
+            return 3
+
+
+def dfs(x, y, visit, direction):
     global answer
     if sum(visit) == N:
-        if x == 0 or y == 0:
+        d = direction
+        if x == 0:
+            d = find_d('x', 0, y)
+        elif y == 0:
+            d = find_d('y', 0, x)
+
+        if d != direction:
             answer += 1
+
         return
-    
+
     for i in range(N):
-        if visit[i] == 0 and (node[i][0] == x or node[i][1] == y):
-            visit[i] = 1
-            dfs(node[i][0],node[i][1],visit)
-            visit[i] = 0
+        if visit[i] == 0:
+            if node[i][0] == x:
+                d = find_d('x', node[i][1], y)
+                if d != direction:
+                    visit[i] = 1
+                    dfs(node[i][0], node[i][1], visit, d)
+                    visit[i] = 0
+
+            elif node[i][1] == y:
+                d = find_d('y', node[i][0], x)
+                if d != direction:
+                    visit[i] = 1
+                    dfs(node[i][0], node[i][1], visit, d)
+                    visit[i] = 0
 
 
 visit = [0 for _ in range(N)]
-dfs(0,0,visit)
+dfs(0, 0, visit, 0)
 print(answer)
